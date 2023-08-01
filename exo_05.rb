f7b78a8b2291e58d3ef3f7301cf6d81c9ea8af9a
fadhima <fadhimatazahra.ndiaye@uadb.edu.sn>
2023-08-01 22:27:05 +0000

    first commit

---
+++ exo_05.rb
@@ -0,0 +1,27 @@
+# affiçage de On va compter le nombre d'heures de travail à THP
+puts "On va compter le nombre d'heures de travail à THP"
+#Travail:550
+puts "Travail:#{10*5*11}"
+#En minutes ça fait : 33000
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+#affiçage de "Et en secondes"
+puts "Et en secondes ?"
+#affiçage 1980000
+puts 10*5*11*60*60
+#affichage Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+#false
+puts 3 + 2 < 5 - 7
+#ça fait combien 3 + 2 ? 5
+puts "ça fait combien 3 + 2 ? #{3+2}"
+#Ça fait combien 5 - 7 ? -2
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+#ok, c'est faux alors !
+#C'est drôle ça, faisons-en plus :
+puts "C'est drôle ça, faisons-en plus :"
+#true
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+#true
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+#false
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
