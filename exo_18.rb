70a51ec329c73ada8300c93b06806d05ccccec3f
fadhima <fadhimatazahra.ndiaye@uadb.edu.sn>
2023-08-02 16:53:23 +0000

    exo_18

---
+++ exo_18.rb
@@ -0,0 +1,10 @@
+list = Array.new
+for
+    n in (1..50)
+    if n < 10
+            list.push("jean.dupont.0#{n}@email.fr")
+    else
+        list.push("jean.dupont.#{n}@email.fr")
+    end
+end
+puts list
